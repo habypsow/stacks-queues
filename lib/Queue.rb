9e79afe0954539625c1a6339431e8604c57b0894
Haby Randall <sow.haby@gmail.com>
2017-02-25 13:57:31 -0800

    Primary Requirements done

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @q = Array.new
   end
 
   def enqueue(element)
+    @q.push(element)
   end
 
   def dequeue
+     @q.shift
   end
 
   def front
+    @q.first
   end
 
   def size
+    return @q.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
+    return @q.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,24 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
   end
 
   def pop
+    @store.pop
+
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
